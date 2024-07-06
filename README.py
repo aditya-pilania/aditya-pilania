c01330a052894007a1253f38cc4dffd5ec394370
Aditya Piania <adityapilaniaoffic@gmail.com>
2024-07-06 15:29:12 +0530

    Update and rename README.md to README.py

--- README.py
+++ README.py
@@ -1,3 +1,4 @@
+"""
 -👋 Hi, I’m @aditya-pilania
 -👀 I’m interested in data science, game development, and software development.
 -🌱 I’m currently learning data science with python and persuing Bachelor's of Computer Application final year student.
@@ -6,7 +7,8 @@
 -😄 Pronouns: He/Him
 -⚡ Fun fact: I love solving puzzles, being the best in gully cricket, playing competetive eSports and have a knack for playing the guitar!
 
-<!---
-aditya-pilania/aditya-pilania is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+# <!---
+# aditya-pilania/aditya-pilania is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+# You can click the Preview link to take a look at your changes.
+# --->
+"""
